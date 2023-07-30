06b27a9941b0e05022998b68ffcc26fd0a1a45b9
pavel.novikov <pavel.novikov@benefitsystem.pl>
2023-07-30 15:19:42 +0200

    main: change in template, add todo

--- app/src/Service/WeatherService/WeatherService.php
+++ app/src/Service/WeatherService/WeatherService.php
@@ -25,6 +25,18 @@ class WeatherService
 
     public function getWeather(City $city, Country $country): float
     {
+        //TODO: Caching mechanism is implemented but tbh I don't get clue of adding cache here. Weather data is pretty dynamic data and may vary from request to request
+        //TODO: moreover in task we want to keep results of searching in the database but because of caching we will not be able to save data a lot of data to database, only requests after cache expire will be saved
+        //TODO: Here I add example of how can I cache data in Symfony but I decide to not use cache
+//        $cacheKey = $this->getCacheKey($city, $country);
+//
+//        return $this->cache->get($cacheKey, function () use ($city) {
+//            $ambeeClientData = $this->ambeeClient->getWeather($city->getLatitude(), $city->getLongitude());
+//            $openWeatherClientData = $this->openWeatherMapClient->getWeather($city->getLatitude(), $city->getLongitude());
+//
+//            return ($ambeeClientData + $openWeatherClientData) / 2;
+//        });
+
         $ambeeClientData = $this->ambeeClient->getWeather($city->getLatitude(), $city->getLongitude());
         $openWeatherClientData = $this->openWeatherMapClient->getWeather($city->getLatitude(), $city->getLongitude());
         $currentAverageTemperature = ($ambeeClientData + $openWeatherClientData) / 2;
